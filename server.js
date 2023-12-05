2d42cd77dbc6144950766f67bb8446fd2b8d2853
amankumar2509 <asr250901@gmail.com>
2023-12-05 19:32:33 +0530

    added view template

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const connectDB=require('./config/db');
 
 
 const PORT=process.env.PORT|| 5000;
-
-
 connectDB();
+
+//template engine
+
+app.set('views',path.join(__dirname,'/views'));
+app.set('view engine','ejs');
+
+
 //routes
 
 const fileRoutes = require('./Routes/file');
 app.use('/api/files', fileRoutes);
+const showRoutes = require('./Routes/show');
+app.use('/files',showRoutes)
 
 
 app.listen(PORT,()=>{
